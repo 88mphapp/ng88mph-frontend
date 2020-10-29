089050814aac89404f9d0a49c88af595c03519d8
Zefram Lou <zeframlou@gmail.com>
2020-10-28 17:26:29 -0700

    Add MPH balance to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ApolloQueryResult } from '@apollo/client/core';
+import { Apollo } from 'apollo-angular';
+import BigNumber from 'bignumber.js';
+import gql from 'graphql-tag';
 import { WalletService } from '../wallet.service';
 
 @Component({
@@ -7,13 +11,60 @@ import { WalletService } from '../wallet.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  mphBalance: BigNumber;
 
-  constructor(public wallet: WalletService) { }
+  constructor(private apollo: Apollo, public wallet: WalletService) {
+    this.resetData();
+  }
 
   ngOnInit(): void {
+    if (this.wallet.connected) {
+      this.loadData();
+    }
+    this.wallet.connectedEvent.subscribe(() => {
+      this.loadData();
+    });
+    this.wallet.errorEvent.subscribe(() => {
+      this.resetData();
+    });
+  }
+
+  loadData(): void {
+    const mphHolderID = this.wallet.userAddress.toLowerCase();
+    const queryString = gql`
+      {
+        mphholder(id: "${mphHolderID}") {
+          mphBalance
+          stakedMPHBalance
+        }
+      }
+    `;
+    this.apollo.query<QueryResult>({
+      query: queryString
+    }).subscribe((x) => this.handleData(x));
+  }
+
+  handleData(queryResult: ApolloQueryResult<QueryResult>): void {
+    if (!queryResult.loading) {
+      const mphHolder = queryResult.data.mphholder;
+      if (mphHolder) {
+        this.mphBalance = new BigNumber(mphHolder.mphBalance).plus(mphHolder.stakedMPHBalance);
+      }
+    }
+  }
+
+  resetData(): void {
+    this.mphBalance = new BigNumber(0);
   }
 
   connectWallet() {
-    this.wallet.connect(() => {}, () => {}, false);
+    this.wallet.connect(() => { }, () => { }, false);
   }
+}
+
+interface QueryResult {
+  mphholder: {
+    mphBalance: number;
+    stakedMPHBalance: number;
+  };
 }
