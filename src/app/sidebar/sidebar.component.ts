2a479e178d28ae79cd964e94c25f986868386d6e
Zefram Lou <zeframlou@gmail.com>
2020-10-28 17:34:24 -0700

    Add wallet connection and MPH balance to sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApolloQueryResult } from '@apollo/client/core';
+import { Apollo } from 'apollo-angular';
+import BigNumber from 'bignumber.js';
+import gql from 'graphql-tag';
+import { WalletService } from '../wallet.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,11 +12,60 @@ import { Router } from '@angular/router';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  mphBalance: BigNumber;
 
-  constructor(public route: Router) { }
-
-  ngOnInit(): void {
+  constructor(private apollo: Apollo, public route: Router, public wallet: WalletService) {
+    this.resetData();
   }
 
+  ngOnInit(): void {
+    if (this.wallet.connected) {
+      this.loadData();
+    }
+    this.wallet.connectedEvent.subscribe(() => {
+      this.loadData();
+    });
+    this.wallet.errorEvent.subscribe(() => {
+      this.resetData();
+    });
+  }
 
+  loadData(): void {
+    const mphHolderID = this.wallet.userAddress.toLowerCase();
+    const queryString = gql`
+      {
+        mphholder(id: "${mphHolderID}") {
+          mphBalance
+          stakedMPHBalance
+        }
+      }
+    `;
+    this.apollo.query<QueryResult>({
+      query: queryString
+    }).subscribe((x) => this.handleData(x));
+  }
+
+  handleData(queryResult: ApolloQueryResult<QueryResult>): void {
+    if (!queryResult.loading) {
+      const mphHolder = queryResult.data.mphholder;
+      if (mphHolder) {
+        this.mphBalance = new BigNumber(mphHolder.mphBalance).plus(mphHolder.stakedMPHBalance);
+      }
+    }
+  }
+
+  resetData(): void {
+    this.mphBalance = new BigNumber(0);
+  }
+
+  connectWallet() {
+    this.wallet.connect(() => { }, () => { }, false);
+  }
+}
+
+interface QueryResult {
+  mphholder: {
+    mphBalance: number;
+    stakedMPHBalance: number;
+  };
 }
