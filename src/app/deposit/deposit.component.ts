eba738bf863f6574fb2dd04284eda756b2070666
Zefram Lou <zeframlou@gmail.com>
2020-10-29 21:48:55 -0700

    Finish user deposit list, add token icons, make icons colored, update wording

--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -8,11 +8,42 @@ import { ModalDepositComponent } from './modal-deposit/modal-deposit.component';
 import { ModalWithdrawComponent } from './modal-withdraw/modal-withdraw.component';
 import { Router } from '@angular/router';
 import { WalletService } from '../wallet.service';
-import { ContractService } from '../contract.service';
+import { ContractService, PoolInfo } from '../contract.service';
 import { HelpersService } from '../helpers.service';
+import { Timer } from '../timer';
 
 const mockUser = {
   totalMPHEarned: 1e3,
+  pools: [
+    {
+      address: '0x6bA0251940E6c22c1FF5270198a134E3779B2f93',
+      deposits: [
+        {
+          nftID: 1,
+          amount: 1e3,
+          maturationTimestamp: 1609459200,
+          depositTimestamp: 1601510400,
+          interestEarned: 1e2,
+          mintMPHAmount: 1e2,
+          takeBackMPHAmount: 0
+        }
+      ]
+    },
+    {
+      address: '0xF44AfBA5a8F55D54d4509db4baC51a9961B7aA05',
+      deposits: [
+        {
+          nftID: 1,
+          amount: 1e3,
+          maturationTimestamp: 1601510400,
+          depositTimestamp: 1601510400,
+          interestEarned: 1e2,
+          mintMPHAmount: 1e2,
+          takeBackMPHAmount: 0
+        }
+      ]
+    }
+  ],
   totalDeposits: [{
     pool: {
       stablecoin: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
@@ -32,6 +63,7 @@ export class DepositComponent implements OnInit {
   totalInterestUSD: BigNumber;
   totalMPHEarned: BigNumber;
   allPoolList: DPool[];
+  userPools: UserPool[];
 
   constructor(
     private modalService: NgbModal,
@@ -62,18 +94,17 @@ export class DepositComponent implements OnInit {
       {
         user(id: "${userID}") {
           totalMPHEarned
-          deposits(where: { active: true }) {
-            nftID
-            pool {
-              address
+          pools {
+            address
+            deposits(where: { user: "${userID}", active: true, orderBy: nftID }) {
+              nftID
+              amount
+              maturationTimestamp
+              depositTimestamp
+              interestEarned
+              mintMPHAmount
+              takeBackMPHAmount
             }
-            amount
-            maturationTimestamp
-            depositTimestamp
-            interestEarned
-            fundingID
-            mintMPHAmount
-            takeBackMPHAmount
           }
           totalDeposits {
             pool {
@@ -97,12 +128,45 @@ export class DepositComponent implements OnInit {
 
   async handleData(queryResult: ApolloQueryResult<QueryResult>) {
     if (!queryResult.loading) {
-      const user = mockUser;//queryResult.data.user;
+      const user = queryResult.data.user;
       const dpools = queryResult.data.dpools;
       let stablecoinPriceCache = {};
       if (user) {
         // update totalMPHEarned
         this.totalMPHEarned = new BigNumber(user.totalMPHEarned);
+
+        // process user deposit list
+        const userPools: UserPool[] = [];
+        for (const pool of user.pools) {
+          const poolInfo = this.contract.getPoolInfoFromAddress(pool.address);
+          const stablecoin = poolInfo.stablecoin.toLowerCase()
+          let stablecoinPrice = stablecoinPriceCache[stablecoin];
+          if (!stablecoinPrice) {
+            stablecoinPrice = await this.helpers.getTokenPriceUSD(stablecoin);
+            stablecoinPriceCache[stablecoin] = stablecoinPrice;
+          }
+          const userPoolDeposits: Array<UserDeposit> = [];
+          for (const deposit of pool.deposits) {
+            const userPoolDeposit: UserDeposit = {
+              nftID: deposit.nftID,
+              amountToken: new BigNumber(deposit.amount),
+              amountUSD: new BigNumber(deposit.amount).times(stablecoinPrice),
+              countdownTimer: new Timer(deposit.maturationTimestamp, 'down'),
+              interestEarnedToken: new BigNumber(deposit.interestEarned),
+              interestEarnedUSD: new BigNumber(deposit.interestEarned).times(stablecoinPrice),
+              mintMPHAmount: new BigNumber(deposit.mintMPHAmount)
+            }
+            userPoolDeposit.countdownTimer.start();
+            userPoolDeposits.push(userPoolDeposit);
+          }
+
+          const userPool: UserPool = {
+            poolInfo: poolInfo,
+            deposits: userPoolDeposits
+          };
+          userPools.push(userPool);
+        }
+        this.userPools = userPools;
 
         // compute total deposit & interest in USD
         let totalDepositUSD = new BigNumber(0);
@@ -135,7 +199,7 @@ export class DepositComponent implements OnInit {
             stablecoinPriceCache[stablecoin] = stablecoinPrice;
           }
 
-          const dpoolObj = {
+          const dpoolObj: DPool = {
             name: poolInfo.name,
             protocol: poolInfo.protocol,
             stablecoin: poolInfo.stablecoin,
@@ -144,7 +208,7 @@ export class DepositComponent implements OnInit {
             totalDepositToken: new BigNumber(pool.totalActiveDeposit),
             totalDepositUSD: new BigNumber(pool.totalActiveDeposit).times(stablecoinPrice),
             oneYearInterestRate: new BigNumber(pool.oneYearInterestRate).times(100),
-          } as DPool;
+          };
           allPoolList.push(dpoolObj);
         }
         this.allPoolList = allPoolList;
@@ -157,6 +221,7 @@ export class DepositComponent implements OnInit {
     this.totalInterestUSD = new BigNumber(0);
     this.totalMPHEarned = new BigNumber(0);
     this.allPoolList = [];
+    this.userPools = [];
   }
 
   openDepositModal() {
@@ -172,18 +237,18 @@ export class DepositComponent implements OnInit {
 interface QueryResult {
   user: {
     totalMPHEarned: number;
-    deposits: {
-      nftID: number;
-      pool: {
-        address: string;
-      }
-      amount: number;
-      maturationTimestamp: number;
-      depositTimestamp: number;
-      interestEarned: number;
-      fundingID: number;
-      mintMPHAmount: number;
-      takeBackMPHAmount: number;
+    pools: {
+      address: string;
+      deposits: {
+        nftID: number;
+        amount: number;
+        maturationTimestamp: number;
+        depositTimestamp: number;
+        interestEarned: number;
+        fundingID: number;
+        mintMPHAmount: number;
+        takeBackMPHAmount: number;
+      }[];
     }[];
     totalDeposits: {
       pool: {
@@ -210,4 +275,19 @@ interface DPool {
   totalDepositToken: BigNumber;
   totalDepositUSD: BigNumber;
   oneYearInterestRate: BigNumber;
+}
+
+interface UserPool {
+  poolInfo: PoolInfo;
+  deposits: UserDeposit[];
+}
+
+interface UserDeposit {
+  nftID: number;
+  amountToken: BigNumber;
+  amountUSD: BigNumber;
+  countdownTimer: Timer;
+  interestEarnedToken: BigNumber;
+  interestEarnedUSD: BigNumber;
+  mintMPHAmount: BigNumber;
 }
--- src/app/timer.ts
+++ src/app/timer.ts
@@ -1,48 +1,52 @@
 export class Timer {
   target: number;
+  countDirection: 'down' | 'up';
+  days: string;
   hours: string;
   minutes: string;
   seconds: string;
+  running: boolean;
   timeIntervalRef: NodeJS.Timeout;
 
-  constructor(target: number) {
+  constructor(target: number, countDirection: 'down' | 'up') {
     this.target = target;
+    this.countDirection = countDirection;
+    this.days = '--';
     this.hours = '--';
     this.minutes = '--';
     this.seconds = '--';
+    this.running = false;
   }
 
   start() {
+    this.running = true;
     this.timeIntervalRef = setInterval(() => {
       this.update();
     }, 1000);
   }
 
   stop() {
+    this.running = false;
     clearInterval(this.timeIntervalRef);
   }
 
   update() {
-    if (+this.hours === 0 && +this.minutes === 0 && +this.seconds === 0) {
+    if ((this.countDirection === 'down' && Date.now() / 1e3 > this.target)
+      || (this.countDirection === 'up' && Date.now() / 1e3 < this.target)) {
       this.stop();
       return;
     }
 
+    const DAY = 24 * 60 * 60;
     const HOUR = 60 * 60;
     const MINUTE = 60;
     const now = Math.floor(Date.now() / 1e3);
-    let hours = Math.floor(Math.abs((now - this.target) / HOUR)).toString();
-    if (hours.length < 2) {
-      hours = `0${hours}`;
-    }
+    let days = Math.floor(Math.abs((now - this.target) / DAY)).toString();
+    let hours = Math.floor(Math.abs((now - this.target) % DAY / HOUR)).toString();
     let minutes = Math.floor(Math.abs((now - this.target) % HOUR / MINUTE)).toString();
-    if (minutes.length < 2) {
-      minutes = `0${minutes}`;
-    }
     let seconds = Math.floor(Math.abs((now - this.target) % MINUTE)).toString();
-    if (seconds.length < 2) {
-      seconds = `0${seconds}`;
-    }
+
+    this.days = days;
     this.hours = hours;
     this.minutes = minutes;
     this.seconds = seconds;
