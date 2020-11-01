b6233c1a512d999a08fc075c621328fb511febc7
Zefram Lou <zeframlou@gmail.com>
2020-10-31 21:54:50 -0700

    Implement deposit modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,7 +35,8 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
