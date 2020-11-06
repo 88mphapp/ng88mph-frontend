e48a3c4e04d156019acddcebc55badd1def9bc4f
Zefram Lou <zeframlou@gmail.com>
2020-11-05 22:14:26 -0800

    Implement bonds ROI calculator

--- src/app/bonds/bonds.component.ts
+++ src/app/bonds/bonds.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApolloQueryResult, gql } from '@apollo/client';
 import { Apollo } from 'apollo-angular';
 import BigNumber from 'bignumber.js';
+import { ConstantsService } from '../constants.service';
 import { ContractService, PoolInfo } from '../contract.service';
 import { HelpersService } from '../helpers.service';
 import { WalletService } from '../wallet.service';
@@ -43,13 +44,27 @@ export class BondsComponent implements OnInit {
   totalDeficitFundedUSD: BigNumber;
   totalCurrentDepositUSD: BigNumber;
   totalInterestUSD: BigNumber;
-  selectedPoolInfo: PoolInfo;
+
+  selectedPool: DPool;
+  floatingRatePrediction: BigNumber;
+  numDepositsToFund: string;
+  numFundableDeposits: number;
+  fundableDeposits: Deposit[];
+  debtToFundToken: BigNumber;
+  debtToFundUSD: BigNumber;
+  amountToEarnOnToken: BigNumber;
+  amountToEarnOnUSD: BigNumber;
+  mphRewardAmount: BigNumber;
+  estimatedProfitToken: BigNumber;
+  estimatedROI: BigNumber;
+  estimatedProfitUSD: BigNumber;
 
   constructor(
     private apollo: Apollo,
     public wallet: WalletService,
     public contract: ContractService,
-    public helpers: HelpersService
+    public helpers: HelpersService,
+    public constants: ConstantsService
   ) {
     this.resetData();
   }
@@ -65,8 +80,6 @@ export class BondsComponent implements OnInit {
   }
 
   loadData(): void {
-    this.selectPool(this.contract.getPoolInfoList()[0].name);
-
     const funderID = this.wallet.connected ? this.wallet.userAddress.toLowerCase() : '';
     const queryString = gql`
       {
@@ -96,6 +109,7 @@ export class BondsComponent implements OnInit {
         dpools {
           address
           surplus
+          unfundedDepositAmount
           oneYearInterestRate
           latestFundedDeposit: deposits(where: { fundingID_gt: 0 }, orderBy: nftID, orderDirection: desc, first: 1) {
             nftID
@@ -113,7 +127,7 @@ export class BondsComponent implements OnInit {
 
   async handleData(queryResult: ApolloQueryResult<QueryResult>) {
     if (!queryResult.loading) {
-      const funder = queryResult.data.funder;
+      const funder = mockFunder;//queryResult.data.funder;
       const dpools = queryResult.data.dpools;
       let stablecoinPriceCache = {};
 
@@ -179,8 +193,10 @@ export class BondsComponent implements OnInit {
 
       if (dpools) {
         const allPoolList = new Array<DPool>(0);
+        const mphMinter = this.contract.getNamedContract('MPHMinter');
         for (const pool of dpools) {
           const poolInfo = this.contract.getPoolInfoFromAddress(pool.address);
+          const stablecoinPrecision = Math.pow(10, poolInfo.stablecoinDecimals);
 
           const stablecoin = poolInfo.stablecoin.toLowerCase()
           let stablecoinPrice = stablecoinPriceCache[stablecoin];
@@ -189,22 +205,33 @@ export class BondsComponent implements OnInit {
             stablecoinPriceCache[stablecoin] = stablecoinPrice;
           }
 
+          // get MPH reward amount
+          const poolMintingMultiplier = new BigNumber(await mphMinter.methods.poolMintingMultiplier(poolInfo.address).call()).div(this.constants.PRECISION);
+          const poolFunderRewardMultiplier = new BigNumber(await mphMinter.methods.poolFunderRewardMultiplier(poolInfo.address).call()).div(this.constants.PRECISION);
+          const mphRewardPerToken = poolMintingMultiplier.times(stablecoinPrecision).div(this.constants.PRECISION).times(poolFunderRewardMultiplier).div(this.constants.PRECISION);
+
           const latestFundedDeposit = pool.latestFundedDeposit.length ? pool.latestFundedDeposit[0].nftID : 0;
           const latestDeposit = pool.latestDeposit.length ? pool.latestDeposit[0].nftID : 0;
           const dpoolObj: DPool = {
             name: poolInfo.name,
+            address: poolInfo.address,
             protocol: poolInfo.protocol,
             stablecoin: poolInfo.stablecoin,
             stablecoinSymbol: poolInfo.stablecoinSymbol,
+            stablecoinDecimals: poolInfo.stablecoinDecimals,
             iconPath: poolInfo.iconPath,
             surplus: new BigNumber(pool.surplus),
             oneYearInterestRate: new BigNumber(pool.oneYearInterestRate).times(100),
             latestFundedDeposit: latestFundedDeposit,
-            latestDeposit: latestDeposit
+            latestDeposit: latestDeposit,
+            unfundedDepositAmount: new BigNumber(pool.unfundedDepositAmount),
+            mphRewardPerToken: mphRewardPerToken
           };
           allPoolList.push(dpoolObj);
         }
         this.allPoolList = allPoolList;
+
+        this.selectPool(0);
       }
     }
   }
@@ -214,13 +241,117 @@ export class BondsComponent implements OnInit {
     this.totalCurrentDepositUSD = new BigNumber(0);
     this.totalInterestUSD = new BigNumber(0);
     this.totalMPHEarned = new BigNumber(0);
+    this.floatingRatePrediction = new BigNumber(0);
     this.allPoolList = [];
     this.funderPools = [];
+    this.numDepositsToFund = 'All';
+    this.numFundableDeposits = 0;
+    this.fundableDeposits = [];
+    this.debtToFundToken = new BigNumber(0);
+    this.debtToFundUSD = new BigNumber(0);
+    this.amountToEarnOnToken = new BigNumber(0);
+    this.amountToEarnOnUSD = new BigNumber(0);
+    this.mphRewardAmount = new BigNumber(0);
+    this.estimatedProfitToken = new BigNumber(0);
+    this.estimatedProfitUSD = new BigNumber(0);
+    this.estimatedROI = new BigNumber(0);
   }
 
-  selectPool(poolName: string) {
-    this.selectedPoolInfo = this.contract.getPoolInfo(poolName);
+  selectPool(poolIdx: number) {
+    this.selectedPool = this.allPoolList[poolIdx];
+    this.floatingRatePrediction = this.selectedPool.oneYearInterestRate.times(4 / 3);
+    this.numFundableDeposits = Math.min(this.selectedPool.latestDeposit - this.selectedPool.latestFundedDeposit - 1, 20);
 
+    const poolID = this.selectedPool.address.toLowerCase();
+    const queryString = gql`
+      {
+        dpool(id: "${poolID}") {
+          deposits(where: { nftID_gt: ${this.selectedPool.latestFundedDeposit} }, orderBy: nftID) {
+            nftID
+            amount
+            active
+            maturationTimestamp
+            interestEarned
+            initialMoneyMarketIncomeIndex
+          }
+        }
+      }
+    `;
+    this.apollo.query<FundableDepositsQuery>({
+      query: queryString
+    }).subscribe(async (x) => {
+      const fundableDeposits = [];
+      const pool = this.contract.getPool(this.allPoolList[poolIdx].name);
+      const moneyMarketIncomeIndex = new BigNumber(await pool.methods.moneyMarketIncomeIndex().call()).div(this.constants.PRECISION);
+      for (const deposit of x.data.dpool.deposits) {
+        const surplus = moneyMarketIncomeIndex.div(deposit.initialMoneyMarketIncomeIndex).minus(1).times(deposit.amount).minus(deposit.interestEarned);
+        const parsedDeposit: Deposit = {
+          nftID: deposit.nftID,
+          amount: new BigNumber(deposit.amount),
+          active: deposit.active,
+          maturationTimestamp: deposit.maturationTimestamp,
+          interestEarned: new BigNumber(deposit.interestEarned),
+          surplus: surplus
+        }
+        fundableDeposits.push(parsedDeposit);
+      }
+      this.fundableDeposits = fundableDeposits;
+
+      this.updateNumDepositsToFund(this.numFundableDeposits + 1);
+    });
+  }
+
+  updateFloatingRatePrediction(newPrediction: number) {
+    this.floatingRatePrediction = new BigNumber(newPrediction);
+    this.updateEstimatedROI();
+  }
+
+  async updateNumDepositsToFund(newNum: number) {
+    if (newNum > this.numFundableDeposits) {
+      // fund all deposits
+      this.numDepositsToFund = 'All';
+      this.debtToFundToken = this.selectedPool.surplus.times(-1);
+      this.amountToEarnOnToken = this.selectedPool.unfundedDepositAmount;
+    } else {
+      this.numDepositsToFund = newNum.toString();
+      let debtToFundToken = new BigNumber(0);
+      let amountToEarnOnToken = new BigNumber(0);
+      for (const deposit of this.fundableDeposits.slice(0, newNum)) {
+        if (!deposit.active) continue;
+        debtToFundToken = debtToFundToken.plus(deposit.surplus.times(-1));
+        amountToEarnOnToken = amountToEarnOnToken.plus(deposit.amount);
+      }
+      this.debtToFundToken = debtToFundToken;
+      this.amountToEarnOnToken = amountToEarnOnToken;
+    }
+    const stablecoinPrice = await this.helpers.getTokenPriceUSD(this.selectedPool.stablecoin);
+    this.debtToFundUSD = this.debtToFundToken.times(stablecoinPrice);
+    this.amountToEarnOnUSD = this.amountToEarnOnToken.times(stablecoinPrice);
+
+    this.mphRewardAmount = this.selectedPool.mphRewardPerToken.times(this.debtToFundToken);
+
+    this.updateEstimatedROI();
+  }
+
+  async updateEstimatedROI() {
+    // compute estimated ROI
+    const estimatedFloatingRate = this.floatingRatePrediction.div(100);
+    let estimatedInterest = new BigNumber(0);
+    const now = Date.now() / 1e3;
+    for (const deposit of this.fundableDeposits) {
+      if (!deposit.active) continue;
+      if (deposit.maturationTimestamp < now) {
+        // Deposit unlocked but not withdrawn
+        // Add surplus
+        estimatedInterest = estimatedInterest.plus(BigNumber.max(deposit.surplus, 0));
+      } else {
+        const depositInterest = deposit.amount.times(estimatedFloatingRate).times(deposit.maturationTimestamp - now).div(this.constants.YEAR_IN_SEC);
+        estimatedInterest = estimatedInterest.plus(depositInterest);
+      }
+    }
+    this.estimatedProfitToken = estimatedInterest.minus(this.debtToFundToken);
+    this.estimatedProfitUSD = this.estimatedProfitToken.times(await this.helpers.getTokenPriceUSD(this.selectedPool.stablecoin.toLowerCase()));
+    this.estimatedROI = this.estimatedProfitToken.div(this.debtToFundToken).times(100);
   }
 }
 
@@ -251,6 +382,7 @@ interface QueryResult {
   dpools: {
     address: string;
     surplus: number;
+    unfundedDepositAmount: number;
     oneYearInterestRate: number;
     latestFundedDeposit: {
       nftID: number;
@@ -259,6 +391,19 @@ interface QueryResult {
       nftID: number;
     }[];
   }[];
+}
+
+interface FundableDepositsQuery {
+  dpool: {
+    deposits: {
+      nftID: number;
+      amount: number;
+      active: boolean;
+      maturationTimestamp: number;
+      interestEarned: number;
+      initialMoneyMarketIncomeIndex: number;
+    }[];
+  };
 }
 
 interface FunderPool {
@@ -277,14 +422,27 @@ interface Funding {
   mintMPHAmount: BigNumber;
 }
 
+interface Deposit {
+  nftID: number;
+  amount: BigNumber;
+  active: boolean;
+  maturationTimestamp: number;
+  interestEarned: BigNumber;
+  surplus: BigNumber;
+}
+
 interface DPool {
   name: string;
+  address: string;
   protocol: string;
   stablecoin: string;
   stablecoinSymbol: string;
+  stablecoinDecimals: number;
   iconPath: string;
   latestFundedDeposit: number;
   latestDeposit: number;
   surplus: BigNumber;
   oneYearInterestRate: BigNumber;
+  unfundedDepositAmount: BigNumber;
+  mphRewardPerToken: BigNumber;
 }
