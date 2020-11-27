a27c38430708343762912cc1a399f8d82d3d8914
Zefram Lou <zeframlou@gmail.com>
2020-11-27 11:54:55 -0800

    Implement TVL stats in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,7 @@ import BigNumber from 'bignumber.js';
 import gql from 'graphql-tag';
 import { ConstantsService } from '../constants.service';
 import { ContractService } from '../contract.service';
+import { HelpersService } from '../helpers.service';
 import { WalletService } from '../wallet.service';
 
 @Component({
@@ -14,51 +15,98 @@ import { WalletService } from '../wallet.service';
 })
 export class HeaderComponent implements OnInit {
   mphBalance: BigNumber;
+  depositValueLocked: BigNumber;
+  farmingValueLocked: BigNumber;
+  mphPriceUSD: BigNumber;
 
   constructor(private apollo: Apollo, public wallet: WalletService, public contract: ContractService,
-    public constants: ConstantsService) {
-    this.resetData();
+    public constants: ConstantsService, public helpers: HelpersService) {
+    this.resetData(true, true);
   }
 
   ngOnInit(): void {
+    this.loadData(this.wallet.connected, true);
     this.wallet.connectedEvent.subscribe(() => {
-      this.resetData();
-      this.loadData();
+      this.resetData(true, true);
+      this.loadData(true, true);
     });
     this.wallet.disconnectedEvent.subscribe(() => {
-      this.resetData();
+      this.resetData(true, false);
     });
   }
 
-  async loadData() {
-    /*const mphHolderID = this.wallet.connected ? this.wallet.userAddress.toLowerCase() : '';
+  async loadData(loadUser: boolean, loadGlobal: boolean) {
+    const mphHolderID = this.wallet.connected ? this.wallet.userAddress.toLowerCase() : '';
     const queryString = gql`
-      {
+    {
+      ${loadUser ? `
         mphholder(id: "${mphHolderID}") {
           id
           mphBalance
           stakedMPHBalance
         }
-      }
+      ` : ''}
+      ${loadGlobal ? `
+        dpools {
+          id
+          stablecoin
+          totalActiveDeposit
+        }
+      ` : ''}
+    }
     `;
     this.apollo.query<QueryResult>({
       query: queryString
-    }).subscribe((x) => this.handleData(x));*/
-    const mphToken = this.contract.getNamedContract('MPHToken')
-    this.mphBalance = new BigNumber(await mphToken.methods.balanceOf(this.wallet.userAddress).call()).div(this.constants.PRECISION);
+    }).subscribe((x) => this.handleData(x));
+
+    if (loadGlobal) {
+      const readonlyWeb3 = this.wallet.readonlyWeb3();
+      const rewards = this.contract.getNamedContract('Farming', readonlyWeb3);
+      const totalStakedMPHLPBalance = new BigNumber(await rewards.methods.totalSupply().call()).div(this.constants.PRECISION);
+      const mphLPPriceUSD = await this.helpers.getMPHLPPriceUSD();
+      this.farmingValueLocked = totalStakedMPHLPBalance.times(mphLPPriceUSD);
+      this.mphPriceUSD = await this.helpers.getMPHPriceUSD();
+    }
   }
 
   handleData(queryResult: ApolloQueryResult<QueryResult>): void {
     if (!queryResult.loading) {
       const mphHolder = queryResult.data.mphholder;
+      const dpools = queryResult.data.dpools;
       if (mphHolder) {
         this.mphBalance = new BigNumber(mphHolder.mphBalance).plus(mphHolder.stakedMPHBalance);
+      }
+      if (dpools) {
+        let totalDepositUSD = new BigNumber(0);
+        let stablecoinPriceCache = {};
+        Promise.all(
+          dpools.map(async pool => {
+            let stablecoinPrice = stablecoinPriceCache[pool.stablecoin];
+            if (!stablecoinPrice) {
+              stablecoinPrice = await this.helpers.getTokenPriceUSD(pool.stablecoin);
+              stablecoinPriceCache[pool.stablecoin] = stablecoinPrice;
+            }
+
+            const poolDepositUSD = new BigNumber(pool.totalActiveDeposit).times(stablecoinPrice);
+            totalDepositUSD = totalDepositUSD.plus(poolDepositUSD);
+          })
+        ).then(() => {
+          this.depositValueLocked = totalDepositUSD;
+        });
       }
     }
   }
 
-  resetData(): void {
-    this.mphBalance = new BigNumber(0);
+  resetData(resetUser: boolean, resetGlobal: boolean): void {
+    if (resetUser) {
+      this.mphBalance = new BigNumber(0);
+    }
+
+    if (resetGlobal) {
+      this.depositValueLocked = new BigNumber(0);
+      this.farmingValueLocked = new BigNumber(0);
+      this.mphPriceUSD = new BigNumber(0);
+    }
   }
 
   connectWallet() {
@@ -72,4 +120,9 @@ interface QueryResult {
     mphBalance: number;
     stakedMPHBalance: number;
   };
+  dpools: {
+    id: string;
+    stablecoin: string;
+    totalActiveDeposit: number;
+  }[];
 }
