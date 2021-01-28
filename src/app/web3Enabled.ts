e90f6ccd626f52ac7d7dd81ca6727b2369ce94b7
Zefram Lou <zeframlou@gmail.com>
2021-01-28 10:12:11 -0800

    Reenable walletconnect

--- src/app/web3Enabled.ts
+++ src/app/web3Enabled.ts
@@ -60,14 +60,14 @@ export class Web3Enabled {
 
       const wallets = [
         genericMobileWalletConfig,
-        /*{
+        {
           walletName: 'walletConnect',
           rpc: {
             [this.networkID]: this.alchemyEndpoint
           },
           networkId: this.networkID,
           preferred: true
-        },*/
+        },
         {
           walletName: 'ledger',
           rpcUrl: this.alchemyEndpoint,
@@ -84,8 +84,7 @@ export class Web3Enabled {
           walletName: 'walletLink',
           rpcUrl: this.alchemyEndpoint,
           appName: '88mph',
-          appLogoUrl: 'https://88mph.app/docs/img/88mph-logo-dark.png',
-          preferred: true
+          appLogoUrl: 'https://88mph.app/docs/img/88mph-logo-dark.png'
         },
         {
           walletName: 'fortmatic',
