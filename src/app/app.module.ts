70d10a3b0a9a7ae359620af97be4dbcf46ed2960
0xSzeth <0xSzeth@gmail.com>
2021-05-02 20:15:47 -0400

    Create mph-supply-distribution component scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,6 +34,7 @@ import { WithdrawZeroCouponBondComponent } from './zero-coupon-bonds/modal-zero-
 import { FarmZeroCouponBondComponent } from './zero-coupon-bonds/modal-zero-coupon-bond-info/farm-zero-coupon-bond/farm-zero-coupon-bond.component';
 import { ModalStakeZCBLPComponent } from './zero-coupon-bonds/modal-zero-coupon-bond-info/farm-zero-coupon-bond/modal-stake-zcblp/modal-stake-zcblp.component';
 import { ModalUnstakeZCBLPComponent } from './zero-coupon-bonds/modal-zero-coupon-bond-info/farm-zero-coupon-bond/modal-unstake-zcblp/modal-unstake-zcblp.component';
+import { MphSupplyDistributionComponent } from './stats/components/mph-supply-distribution/mph-supply-distribution.component';
 
 @NgModule({
   declarations: [
@@ -64,7 +65,8 @@ import { ModalUnstakeZCBLPComponent } from './zero-coupon-bonds/modal-zero-coupo
     WithdrawZeroCouponBondComponent,
     FarmZeroCouponBondComponent,
     ModalStakeZCBLPComponent,
-    ModalUnstakeZCBLPComponent
+    ModalUnstakeZCBLPComponent,
+    MphSupplyDistributionComponent
   ],
   imports: [
     BrowserModule,
