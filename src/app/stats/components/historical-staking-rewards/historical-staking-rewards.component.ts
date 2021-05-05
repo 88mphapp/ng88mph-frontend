ddca7aa5d5ce778fb9148eed69dcc740f339268f
0xSzeth <0xSzeth@gmail.com>
2021-05-05 19:41:30 -0400

    Create scaffolding for historical-staking-rewards component

---
+++ src/app/stats/components/historical-staking-rewards/historical-staking-rewards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historical-staking-rewards',
+  templateUrl: './historical-staking-rewards.component.html',
+  styleUrls: ['./historical-staking-rewards.component.css']
+})
+export class HistoricalStakingRewardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
