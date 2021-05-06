729eebbabb58a437c8218258018543f11aa41fca
0xSzeth <0xSzeth@gmail.com>
2021-05-06 11:45:42 -0400

    Add scaffolding for historical fixed interest rates component

---
+++ src/app/stats/components/historical-fixed-interest-rates/historical-fixed-interest-rates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historical-fixed-interest-rates',
+  templateUrl: './historical-fixed-interest-rates.component.html',
+  styleUrls: ['./historical-fixed-interest-rates.component.css']
+})
+export class HistoricalFixedInterestRatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
