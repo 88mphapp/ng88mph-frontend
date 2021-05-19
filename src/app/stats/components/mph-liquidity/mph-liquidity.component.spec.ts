bd3a22025d1b7efda0d14b3cc2c63849582fc65f
0xSzeth <0xSzeth@gmail.com>
2021-05-19 14:31:34 -0400

    Add implementation for mph liquidity (no Bancor)

---
+++ src/app/stats/components/mph-liquidity/mph-liquidity.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MphLiquidityComponent } from './mph-liquidity.component';
+
+describe('MphLiquidityComponent', () => {
+  let component: MphLiquidityComponent;
+  let fixture: ComponentFixture<MphLiquidityComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MphLiquidityComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MphLiquidityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/stats/components/mph-liquidity/mph-liquidity.component.ts
@@ -0,0 +1,199 @@
+import { Component, OnInit } from '@angular/core';
+import { ApolloQueryResult } from '@apollo/client/core';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import BigNumber from 'bignumber.js';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { TimeSeriesService } from 'src/app/timeseries.service';
+import { ConstantsService } from 'src/app/constants.service';
+import { HelpersService } from 'src/app//helpers.service';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-mph-liquidity',
+  templateUrl: './mph-liquidity.component.html',
+  styleUrls: ['./mph-liquidity.component.css']
+})
+export class MphLiquidityComponent implements OnInit {
+
+  FIRST_INDEX: number = 1609372800; // Jan 1 2021 @ 00:00 UTC
+
+  // data variables
+  timeseriesdata: number[][] = [];
+  timestamps: number[] = [];
+  readable: string[] = [];
+  blocks: number[] = [];
+  uniswap: number[] = [];
+  sushiswap: number[] = [];
+  bancor: number[] = [];
+
+  // chart variables
+  public barChartOptions;
+  public barChartLabels;
+  public barChartType;
+  public barChartLegend;
+  public barChartData;
+
+  constructor(
+    private apollo: Apollo,
+    public helpers: HelpersService,
+    public constants: ConstantsService,
+    public timeseries: TimeSeriesService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.drawChart();
+  }
+
+  async drawChart() {
+    await this.loadData();
+
+    // then draw the chart
+    this.barChartOptions = {
+      scaleShowVerticalLines: false,
+      responsive: true,
+      scales: {
+          xAxes: [{ stacked: true }],
+          yAxes: [{ stacked: true }]
+      }
+    };
+    this.barChartLabels = this.readable;
+    this.barChartType = 'bar';
+    this.barChartLegend = false;
+    this.barChartData = [
+      {
+      data: this.uniswap,
+      label: "Uniswap",
+      backgroundColor: "rgba(221, 107, 229, 0.3)",
+      borderColor: "rgba(221, 107, 229, 1)",
+      hoverBackgroundColor: "rgba(221, 107, 229, 1)"
+      },
+      {
+        data: this.sushiswap,
+        label: "Sushiswap",
+        backgroundColor: "rgba(3, 184, 255, 0.3)",
+        borderColor: "rgba(3, 184, 255, 1)",
+        hoverBackgroundColor: "rgba(3, 184, 255, 1)"
+      },
+      {
+        data: this.bancor,
+        label: "Bancor",
+        backgroundColor: "rgba(255, 255, 255, 0.3)",
+        borderColor: "rgba(255, 255, 255, 1)",
+        hoverBackgroundColor: "rgba(255, 255, 255, 1)"
+      }
+    ];
+  }
+
+  async loadData() {
+
+    // wait to fetch timeseries data
+    this.timeseriesdata =  await this.timeseries.getCustomTimeSeries(this.FIRST_INDEX, this.constants.DAY_IN_SEC);
+
+    // populate timestamps, blocks, and readable arrays
+    this.timestamps = this.timeseriesdata[0];
+    this.blocks = this.timeseriesdata[1];
+
+    // transform timestamps to readable format
+    let readable: string[] = [];
+    for (let i in this.timestamps) {
+      readable.push((new Date(this.timestamps[i]*1000)).toLocaleString("en-US", {month: "short", day: "numeric"}));
+    }
+    this.readable = readable;
+
+    // load data
+    this.loadUniswap();
+    this.loadSushiswap();
+    this.loadBancor();
+
+  }
+
+  async loadUniswap() {
+    const uniswap = new ApolloClient({
+      uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+      cache: new InMemoryCache(),
+    });
+
+    // buld the query string
+    let queryString = `query Uniswap {`;
+    for (let i = 0; i < this.blocks.length; i++) {
+      queryString +=
+      `t${i}: pair(
+        id: "0x4d96369002fc5b9687ee924d458a7e5baa5df34e",
+        block: {
+          number: ${this.blocks[i]}
+        }
+      ) {
+        reserveUSD
+      }`;
+    }
+    queryString += `}`;
+    const query = gql`${queryString}`;
+
+    uniswap.query<QueryResult>({
+      query: query
+    }).then(result =>
+      this.handleUniswapData(result)
+    );
+  }
+
+  async loadSushiswap() {
+    const sushiswap = new ApolloClient({
+      uri: 'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
+      cache: new InMemoryCache(),
+    });
+
+    // buld the query string
+    let queryString = `query Uniswap {`;
+    for (let i = 0; i < this.blocks.length; i++) {
+      queryString +=
+      `t${i}: pair(
+        id: "0xb2c29e311916a346304f83aa44527092d5bd4f0f",
+        block: {
+          number: ${this.blocks[i]}
+        }
+      ) {
+        reserveUSD
+      }`;
+    }
+    queryString += `}`;
+    const query = gql`${queryString}`;
+
+    sushiswap.query<QueryResult>({
+      query: query
+    }).then(result =>
+      this.handleSushiswapData(result)
+    );
+  }
+
+  async loadBancor() {
+    // waiting on Bancor to implement historical data queries to their API
+  }
+
+  handleUniswapData(queryResult: ApolloQueryResult<QueryResult>): void {
+    if (!queryResult.loading) {
+      let result = queryResult.data;
+      for(let i in result) {
+        this.uniswap.push(parseInt(result[i].reserveUSD));
+      }
+    }
+  }
+
+  handleSushiswapData(queryResult: ApolloQueryResult<QueryResult>): void {
+    if (!queryResult.loading) {
+      let result = queryResult.data;
+      for(let i in result) {
+        this.sushiswap.push(parseInt(result[i].reserveUSD));
+      }
+    }
+  }
+
+}
+
+interface QueryResult {
+  pair: {
+    reserveUSD: number;
+  }
+}
