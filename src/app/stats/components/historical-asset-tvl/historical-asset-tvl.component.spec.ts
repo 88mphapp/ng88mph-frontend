8e4ef7773f71501d3dc4ebccd5f6fa4bf8330c5c
0xSzeth <0xSzeth@gmail.com>
2021-05-19 14:10:06 -0400

    Add implementation for historical asset TVL

---
+++ src/app/stats/components/historical-asset-tvl/historical-asset-tvl.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HistoricalAssetTvlComponent } from './historical-asset-tvl.component';
+
+describe('HistoricalAssetTvlComponent', () => {
+  let component: HistoricalAssetTvlComponent;
+  let fixture: ComponentFixture<HistoricalAssetTvlComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HistoricalAssetTvlComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HistoricalAssetTvlComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
