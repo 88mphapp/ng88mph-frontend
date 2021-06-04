a17adaf884d25690c494810052459fd089b65a42
0xSzeth <0xSzeth@gmail.com>
2021-06-04 12:13:35 -0400

    Update ROI calculation and code cleanup

--- src/app/rewards/rewards.component.ts
+++ src/app/rewards/rewards.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ApolloQueryResult } from '@apollo/client/core';
-import { Apollo } from 'apollo-angular';
 import BigNumber from 'bignumber.js';
-import gql from 'graphql-tag';
 import { ContractService } from '../contract.service';
 import { WalletService } from '../wallet.service';
 import { ModalStakeComponent } from './modal-stake/modal-stake.component';
@@ -20,27 +17,15 @@ export class RewardsComponent implements OnInit {
   PERIOD = 7; // 7 days
 
   stakeAmount: BigNumber;
-  unstakedMPHBalance: BigNumber;
   xMPHBalance: BigNumber;
-  xMPHTotalSupply: BigNumber;
+  unstakedMPHBalance: BigNumber;
 
-  stakedMPHBalance: BigNumber;
-  stakedMPHPoolProportion: BigNumber;
-  claimableRewards: BigNumber;
-  rewardPerWeek: BigNumber;
-  totalRewardPerSecond: BigNumber;
-  rewardPerMPHPerSecond: BigNumber;
-  totalStakedMPHBalance: BigNumber;
-  totalHistoricalReward: BigNumber;
-  rewardStartTime: string;
-  rewardEndTime: string;
-  distributionEndTime: string;
-  yearlyROI: BigNumber;
-  monthlyROI: BigNumber;
-  weeklyROI: BigNumber;
-  dailyROI: BigNumber;
   mphPriceUSD: BigNumber;
   xMPHPriceUSD: BigNumber;
+  xMPHTotalSupply: BigNumber;
+  yearlyROI: BigNumber;
+
+  distributionEndTime: string;
   protocolFeesUSD: BigNumber;
   compRewardsToken: BigNumber;
   compRewardsUSD: BigNumber;
@@ -51,7 +36,6 @@ export class RewardsComponent implements OnInit {
   totalRewardsUSD: BigNumber;
 
   constructor(
-    private apollo: Apollo,
     private modalService: NgbModal,
     public wallet: WalletService,
     public contract: ContractService,
@@ -73,131 +57,60 @@ export class RewardsComponent implements OnInit {
   }
 
   async loadData(loadUser: boolean, loadGlobal: boolean) {
-    if (loadGlobal) {
-      const queryString = gql`
-      {
-        mph(id: "0") {
-          id
-          rewardPerMPHPerSecond
-          rewardPerSecond
-          totalHistoricalReward
-        }
-      }
-    `;
-      this.apollo.query<QueryResult>({
-        query: queryString
-      }).subscribe((x) => this.handleData(x));
-    }
 
     const readonlyWeb3 = this.wallet.readonlyWeb3();
     const rewards = this.contract.getNamedContract('Rewards', readonlyWeb3);
     const mph = this.contract.getNamedContract('MPHToken', readonlyWeb3);
     const xmph = this.contract.getNamedContract('xMPHToken', readonlyWeb3);
 
-    if (this.wallet.connected && loadUser && !this.wallet.watching) {
-      rewards.methods.balanceOf(this.wallet.userAddress).call().then(async stakeBalance => {
-        this.stakedMPHBalance = new BigNumber(stakeBalance).div(this.constants.PRECISION);
-        const totalStakedMPHBalance = new BigNumber(await rewards.methods.totalSupply().call()).div(this.constants.PRECISION);
-        this.stakedMPHPoolProportion = this.stakedMPHBalance.div(totalStakedMPHBalance).times(100);
-        if (this.stakedMPHPoolProportion.isNaN()) {
-          this.stakedMPHPoolProportion = new BigNumber(0);
-        }
-        const weekInSeconds = 7 * 24 * 60 * 60;
-        this.rewardPerWeek = this.stakedMPHBalance.times(this.rewardPerMPHPerSecond).times(weekInSeconds);
-      });
+    let address;
+    if (!this.wallet.watching) {
+      address = this.wallet.userAddress;
+    } else {
+      address = this.wallet.watchedAddress;
+    }
 
-      rewards.methods.earned(this.wallet.userAddress).call().then(claimableRewards => {
-        this.claimableRewards = new BigNumber(claimableRewards).div(this.constants.PRECISION);
-      });
-
-      mph.methods.balanceOf(this.wallet.userAddress).call().then(unstakedMPHBalance => {
+    if (loadUser) {
+      mph.methods.balanceOf(address).call().then(unstakedMPHBalance => {
         this.unstakedMPHBalance = new BigNumber(unstakedMPHBalance).div(this.constants.PRECISION);
         this.setStakeAmount(this.unstakedMPHBalance.toFixed(18));
       });
 
-      xmph.methods.balanceOf(this.wallet.userAddress).call().then(xMPHBalance => {
+      xmph.methods.balanceOf(address).call().then(xMPHBalance => {
         this.xMPHBalance = new BigNumber(xMPHBalance).div(this.constants.PRECISION);
       });
-
-    }
-
-    if (loadUser && this.wallet.watching) {
-      rewards.methods.balanceOf(this.wallet.watchedAddress).call().then(async stakeBalance => {
-        this.stakedMPHBalance = new BigNumber(stakeBalance).div(this.constants.PRECISION);
-        const totalStakedMPHBalance = new BigNumber(await rewards.methods.totalSupply().call()).div(this.constants.PRECISION);
-        this.stakedMPHPoolProportion = this.stakedMPHBalance.div(totalStakedMPHBalance).times(100);
-        if (this.stakedMPHPoolProportion.isNaN()) {
-          this.stakedMPHPoolProportion = new BigNumber(0);
-        }
-        const weekInSeconds = 7 * 24 * 60 * 60;
-        this.rewardPerWeek = this.stakedMPHBalance.times(this.rewardPerMPHPerSecond).times(weekInSeconds);
-      });
-
-      rewards.methods.earned(this.wallet.watchedAddress).call().then(claimableRewards => {
-        this.claimableRewards = new BigNumber(claimableRewards).div(this.constants.PRECISION);
-      });
-
-      mph.methods.balanceOf(this.wallet.watchedAddress).call().then(unstakedMPHBalance => {
-        this.unstakedMPHBalance = new BigNumber(unstakedMPHBalance).div(this.constants.PRECISION);
-      });
-
-      xmph.methods.balanceOf(this.wallet.watchedAddress).call().then(xMPHBalance => {
-        this.xMPHBalance = new BigNumber(xMPHBalance).div(this.constants.PRECISION);
-      });
-
     }
 
     if (loadGlobal) {
       // load reward accumulation stats
       this.loadRewardAccumulationStats();
 
-      // load reward start & end time
-      rewards.methods.periodFinish().call().then(endTime => {
-        this.rewardStartTime = new Date((+endTime - this.PERIOD * 24 * 60 * 60) * 1e3).toLocaleString();
-        this.rewardEndTime = new Date(+endTime * 1e3).toLocaleString();
-      });
-
-      // @dev placeholder - delete once xMPH contract has been deployed on mainnet
-      this.distributionEndTime = new Date(1625346438 * 1e3).toLocaleString('en-US', {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
-      // @dev end placeholder
-
-      // @dev uncomment once xMPH contract has been deployed on mainnet
-      // xmph.methods.currentUnlockEndTimestamp().call().then(distributionEndTime => {
-      //   this.distributionEndTime = new Date(distributionEndTime * 1e3).toLocaleString('en-US', {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
-      // });
-
-      rewards.methods.totalSupply().call().then(async totalSupply => {
-        this.totalStakedMPHBalance = new BigNumber(totalSupply).div(this.constants.PRECISION);
-        this.mphPriceUSD = await this.helpers.getMPHPriceUSD();
-        let secondROI = this.totalRewardPerSecond.div(this.totalStakedMPHBalance.times(this.mphPriceUSD)).times(100);
-        if (secondROI.isNaN()) {
-          secondROI = new BigNumber(0);
-        }
-        this.yearlyROI = secondROI.times(this.constants.YEAR_IN_SEC);
-        this.monthlyROI = secondROI.times(this.constants.MONTH_IN_SEC);
-        this.weeklyROI = secondROI.times(this.constants.WEEK_IN_SEC);
-        this.dailyROI = secondROI.times(this.constants.DAY_IN_SEC);
-      });
-
-      // @dev xMPH needs to be listed on coingecko in order for this to work
+      // load MPH and xMPH price data
+      this.mphPriceUSD = await this.helpers.getMPHPriceUSD();
       this.xMPHPriceUSD = new BigNumber(await this.helpers.getTokenPriceUSD(this.constants.XMPH));
 
       xmph.methods.totalSupply().call().then(xMPHTotalSupply => {
         this.xMPHTotalSupply = new BigNumber(xMPHTotalSupply).div(this.constants.PRECISION);
       });
 
+      // load distribution end date
+      // @dev will throw an error until xMPH abi and addresses have been updated
+      xmph.methods.currentUnlockEndTimestamp().call().then(distributionEndTime => {
+        this.distributionEndTime = new Date(distributionEndTime * 1e3).toLocaleString('en-US', {month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric'});
+      });
 
-    }
-  }
-
-  async handleData(queryResult: ApolloQueryResult<QueryResult>) {
-    if (!queryResult.loading) {
-      const mph = queryResult.data.mph;
-      if (mph) {
-        this.rewardPerMPHPerSecond = new BigNumber(mph.rewardPerMPHPerSecond);
-        this.totalRewardPerSecond = new BigNumber(mph.rewardPerSecond);
-        this.totalHistoricalReward = new BigNumber(mph.totalHistoricalReward);
+      // load xMPH rewards data
+      // @dev will throw an error until xMPH abi and addresses have been updated
+      const rewardsEndDate = new BigNumber(await xmph.methods.currentUnlockEndTimestamp().call());
+      const rewardsStartDate = new BigNumber(await xmph.methods.lastRewardTimestamp().call());
+      const rewardsAmount = new BigNumber(await xmph.methods.lastRewardAmount().call()).div(this.constants.PRECISION);
+      const rewardPerSecond = rewardsAmount.div(rewardsEndDate.minus(rewardsStartDate)).times(this.mphPriceUSD);
+      let secondROI = rewardPerSecond.div(this.xMPHTotalSupply.times(this.xMPHPriceUSD)).times(100);
+      if (secondROI.isNaN()) {
+        secondROI = new BigNumber(0);
       }
+      this.yearlyROI = secondROI.times(this.constants.YEAR_IN_SEC);
+
     }
   }
 
@@ -206,24 +119,13 @@ export class RewardsComponent implements OnInit {
       this.stakeAmount = new BigNumber(0);
       this.unstakedMPHBalance = new BigNumber(0);
       this.xMPHBalance = new BigNumber(0);
-      this.stakedMPHBalance = new BigNumber(0);
-      this.stakedMPHPoolProportion = new BigNumber(0);
-      this.claimableRewards = new BigNumber(0);
     }
 
     if (resetGlobal) {
       this.xMPHTotalSupply = new BigNumber(0);
-      this.totalStakedMPHBalance = new BigNumber(0);
-      this.rewardPerMPHPerSecond = new BigNumber(0);
-      this.rewardPerWeek = new BigNumber(0);
-      this.totalRewardPerSecond = new BigNumber(0);
-      this.totalHistoricalReward = new BigNumber(0);
       this.mphPriceUSD = new BigNumber(0);
       this.xMPHPriceUSD = new BigNumber(0);
       this.yearlyROI = new BigNumber(0);
-      this.monthlyROI = new BigNumber(0);
-      this.weeklyROI = new BigNumber(0);
-      this.dailyROI = new BigNumber(0);
       this.protocolFeesUSD = new BigNumber(0);
       this.compRewardsToken = new BigNumber(0);
       this.compRewardsUSD = new BigNumber(0);
@@ -280,7 +182,7 @@ export class RewardsComponent implements OnInit {
       stkaaveRewardsToken = stkaaveRewardsToken.plus(rewardInDumper);
 
       this.stkaaveRewardsToken = stkaaveRewardsToken;
-      const stkaavePriceUSD = await this.helpers.getTokenPriceUSD(this.constants.AAVE); // 1 aave = 1 stkaave
+      const stkaavePriceUSD = await this.helpers.getTokenPriceUSD(this.constants.AAVE);
       this.stkaaveRewardsUSD = stkaaveRewardsToken.times(stkaavePriceUSD);
       this.totalRewardsUSD = this.totalRewardsUSD.plus(stkaaveRewardsToken.times(stkaavePriceUSD));
     });
@@ -358,13 +260,4 @@ export class RewardsComponent implements OnInit {
     return this.wallet.connected && this.xMPHBalance.gt(0);
   }
 
-}
-
-interface QueryResult {
-  mph: {
-    id: string;
-    rewardPerMPHPerSecond: number;
-    rewardPerSecond: number;
-    totalHistoricalReward: number;
-  };
 }
