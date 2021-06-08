2dd31b3024fd5dc34207dfd0c7c2e3460276d35c
0xSzeth <0xSzeth@gmail.com>
2021-06-08 14:37:41 -0400

    Add MPH rewards modal scaffolding

---
+++ src/app/deposit/modal-mph-rewards/modal-mph-rewards.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalMphRewardsComponent } from './modal-mph-rewards.component';
+
+describe('ModalMphRewardsComponent', () => {
+  let component: ModalMphRewardsComponent;
+  let fixture: ComponentFixture<ModalMphRewardsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ModalMphRewardsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModalMphRewardsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
