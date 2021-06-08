948c96a0f5fb0d2d9a1e9c9199cbfc9d68233ad4
0xSzeth <0xSzeth@gmail.com>
2021-06-08 14:59:31 -0400

    Add basic top up modal html

--- src/app/deposit/modal-top-up/modal-top-up.component.ts
+++ src/app/deposit/modal-top-up/modal-top-up.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal-top-up',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalTopUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public activeModal: NgbActiveModal
+  ) { }
 
   ngOnInit(): void {
   }
