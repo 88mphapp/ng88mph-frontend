56e2d39519a58f134556329d1b0540a3658cb315
0xSzeth <0xSzeth@gmail.com>
2021-06-08 14:44:03 -0400

    Add top up modal scaffolding

---
+++ src/app/deposit/modal-top-up/modal-top-up.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModalTopUpComponent } from './modal-top-up.component';
+
+describe('ModalTopUpComponent', () => {
+  let component: ModalTopUpComponent;
+  let fixture: ComponentFixture<ModalTopUpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ModalTopUpComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModalTopUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
