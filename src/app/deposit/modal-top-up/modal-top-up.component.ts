daeb0ae8789d974cec9ab35f2cee4b542b7b99f7
0xSzeth <0xSzeth@gmail.com>
2021-06-09 09:46:00 -0400

    Add wireframe implementation for top up modal

--- src/app/deposit/modal-top-up/modal-top-up.component.ts
+++ src/app/deposit/modal-top-up/modal-top-up.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import BigNumber from 'bignumber.js';
+import { ConstantsService } from 'src/app/constants.service';
+import { HelpersService } from 'src/app/helpers.service';
+import { WalletService } from 'src/app/wallet.service';
+import { ContractService, PoolInfo } from '../../contract.service';
+import { UserPool, UserDeposit } from '../types';
 
 @Component({
   selector: 'app-modal-top-up',
@@ -8,11 +14,41 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ModalTopUpComponent implements OnInit {
 
+  @Input() userDeposit: UserDeposit;
+  @Input() poolInfo: PoolInfo;
+
   constructor(
-    public activeModal: NgbActiveModal
-  ) { }
+    public activeModal: NgbActiveModal,
+    public wallet: WalletService,
+    public contract: ContractService,
+    public helpers: HelpersService,
+    public constants: ConstantsService
+  ) {
+    this.resetData();
+  }
 
   ngOnInit(): void {
+    this.loadData();
+    this.wallet.connectedEvent.subscribe(() => {
+      this.resetData();
+      this.loadData();
+    });
+    this.wallet.disconnectedEvent.subscribe(() => {
+      this.resetData();
+    });
+  }
+
+  loadData(): void {
+    console.log(this.userDeposit);
+    console.log(this.poolInfo);
+  }
+
+  resetData(): void {
+
+  }
+
+  canContinue() {
+
   }
 
 }
