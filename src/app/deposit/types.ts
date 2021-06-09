515f0cacc709baa954036f46074203a8a5831063
0xSzeth <0xSzeth@gmail.com>
2021-06-09 12:18:13 -0400

    Temporary types updates

--- src/app/deposit/types.ts
+++ src/app/deposit/types.ts
@@ -2,6 +2,7 @@ import BigNumber from 'bignumber.js';
 import { PoolInfo, ZeroCouponBondInfo } from '../contract.service';
 import { Timer } from '../timer';
 
+// v2 DPool
 export interface DPool {
   name: string;
   protocol: string;
@@ -15,6 +16,20 @@ export interface DPool {
   tempMPHAPY: BigNumber;
   totalUserDeposits: BigNumber;
   totalUserDepositsUSD: BigNumber;
+}
+
+// v3 DPool
+export interface V3DPool {
+  name: string;
+  protocol: string;
+  stablecoin: string;
+  stablecoinSymbol: string;
+  iconPath: string;
+  oneYearInterestRate: BigNumber;
+  depositorMPHAPR: BigNumber;
+  funderMPHAPR: BigNumber;
+  totalUserDepositToken: BigNumber;
+  totalUserDepositUSD: BigNumber;
 }
 
 export interface UserPool {
@@ -36,6 +51,24 @@ export interface UserDeposit {
   realMPHReward: BigNumber;
   mphAPY: BigNumber;
   tempMPHAPY: BigNumber;
+  virtualTokenTotalSupply: BigNumber;
+  vest: Vest;
+  depositLength: number;
+  interestRate: BigNumber;
+}
+
+// @dev user deposit interface for v3
+export interface V3UserDeposit {
+  nftID: number;
+  virtualTokenTotalSupply: BigNumber;
+  interestRate: BigNumber;
+  feeRate: BigNumber;
+  maturationTimestamp: number;
+  depositTimestamp: number;
+  depositLength: number;
+  averageRecordedIncomeIndex: number;
+  fundingInterestPaid: BigNumber;
+  fundingRefundPaid: BigNumber;
 }
 
 export interface UserZCBPool {
@@ -45,4 +78,13 @@ export interface UserZCBPool {
   amountToken: BigNumber;
   amountUSD: BigNumber;
   maturation: string;
+}
+
+// @dev vest interface for v3
+export interface Vest {
+  id: string;
+  lastUpdateTimestamp: BigNumber;
+  accumulatedAmount: BigNumber;
+  withdrawnAmount: BigNumber;
+  vestAmountPerStablecoinPerSecond: BigNumber;
 }
