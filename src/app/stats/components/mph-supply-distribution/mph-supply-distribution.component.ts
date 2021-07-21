378598f100938bb227e1c3b333a26f98bb126407
0xSzeth <0xSzeth@gmail.com>
2021-07-21 11:32:42 -0600

    Scaffolding for mph supply distribution chart

---
+++ src/app/stats/components/mph-supply-distribution/mph-supply-distribution.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mph-supply-distribution',
+  templateUrl: './mph-supply-distribution.component.html',
+  styleUrls: ['./mph-supply-distribution.component.css'],
+})
+export class MphSupplyDistributionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
