282ff9264110451f8d19a38f7cfd23171696ed35
0xSzeth <0xSzeth@gmail.com>
2021-07-23 14:38:40 -0600

    Scaffolding for NIM chart

---
+++ src/app/stats/components/net-interest-margin/net-interest-margin.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-net-interest-margin',
+  templateUrl: './net-interest-margin.component.html',
+  styleUrls: ['./net-interest-margin.component.css'],
+})
+export class NetInterestMarginComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
