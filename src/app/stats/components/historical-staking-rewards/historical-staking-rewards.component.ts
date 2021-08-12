6ad3ce260b9612e58e8f2b68b3cff9c649428658
0xSzeth <0xSzeth@gmail.com>
2021-08-12 16:03:00 -0600

    Add historical staking rewards chart

---
+++ src/app/stats/components/historical-staking-rewards/historical-staking-rewards.component.ts
@@ -0,0 +1,178 @@
+import { Component, OnInit, NgZone } from '@angular/core';
+import BigNumber from 'bignumber.js';
+import { request, gql } from 'graphql-request';
+import { TimeSeriesService } from 'src/app/timeseries.service';
+import { ConstantsService } from 'src/app/constants.service';
+import { ContractService } from 'src/app/contract.service';
+import { HelpersService } from 'src/app//helpers.service';
+import { WalletService } from 'src/app//wallet.service';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-historical-staking-rewards',
+  templateUrl: './historical-staking-rewards.component.html',
+  styleUrls: ['./historical-staking-rewards.component.css'],
+})
+export class HistoricalStakingRewardsComponent implements OnInit {
+  // constants
+  FIRST_INDEX = {
+    [this.constants.CHAIN_ID.MAINNET]: 1624406400,
+    [this.constants.CHAIN_ID.RINKEBY]: 1624406400,
+  };
+  PERIOD: number = this.constants.DAY_IN_SEC;
+
+  // data variables
+  timeseriesdata: number[][];
+  timestamps: number[];
+  readable: string[];
+  blocks: number[];
+  data: number[];
+
+  // chart variables
+  public barChartOptions;
+  public barChartLabels;
+  public barChartType;
+  public barChartLegend;
+  public barChartData;
+
+  constructor(
+    public helpers: HelpersService,
+    public constants: ConstantsService,
+    public contract: ContractService,
+    public wallet: WalletService,
+    public timeseries: TimeSeriesService,
+    private zone: NgZone
+  ) {}
+
+  ngOnInit(): void {
+    this.resetChart();
+    this.drawChart(this.wallet.networkID);
+    this.wallet.chainChangedEvent.subscribe((networkID) => {
+      this.zone.run(() => {
+        this.resetChart();
+        this.drawChart(networkID);
+      });
+    });
+  }
+
+  resetChart() {
+    this.timeseriesdata = [];
+    this.timestamps = [];
+    this.readable = [];
+    this.blocks = [];
+    this.data = [];
+  }
+
+  async drawChart(networkID: number) {
+    // wait for data to load
+    const loaded = await this.loadData(networkID);
+    if (!loaded) return;
+
+    // then draw the chart
+    this.barChartOptions = {
+      scaleShowVerticalLines: false,
+      responsive: true,
+      scales: {
+        xAxes: [
+          {
+            stacked: true,
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            stacked: true,
+            gridLines: {
+              display: true,
+              color: '#242526',
+            },
+          },
+        ],
+      },
+    };
+    this.barChartLabels = this.readable;
+    this.barChartType = 'bar';
+    this.barChartLegend = false;
+    this.barChartData = [
+      {
+        data: this.data,
+        backgroundColor: 'rgba(44, 123, 229, 0.3)',
+        borderColor: 'rgba(44, 123, 229, 1)',
+        hoverBackgroundColor: 'rgba(44, 123, 229, 1)',
+      },
+    ];
+  }
+
+  async loadData(networkID: number): Promise<boolean> {
+    // wait to fetch timeseries data
+    this.timeseriesdata = await this.timeseries.getCustomTimeSeries(
+      this.FIRST_INDEX[this.wallet.networkID],
+      this.PERIOD
+    );
+
+    // populate timestamps, blocks, and readable arrays
+    this.timestamps = this.timeseriesdata[0];
+    this.blocks = this.timeseriesdata[1];
+
+    // transform timestamps to readable format
+    let readable: string[] = [];
+    for (let i in this.timestamps) {
+      readable.push(
+        new Date(this.timestamps[i] * 1000).toLocaleString('en-US', {
+          month: 'short',
+          day: 'numeric',
+        })
+      );
+    }
+    this.readable = readable;
+
+    // bail if a chain change has occured
+    if (networkID !== this.wallet.networkID) {
+      return false;
+    }
+
+    // then generate the query
+    let queryString = `query HistoricaStakingRewards {`;
+    for (let i = 0; i < this.blocks.length; i++) {
+      queryString += `t${i}: globalStats(
+        id: "0"
+        block: {
+          number: ${this.blocks[i]}
+        }
+      ) {
+        xMPHRewardDistributed
+      }`;
+    }
+    queryString += `}`;
+    const query = gql`
+      ${queryString}
+    `;
+
+    // then run the query
+    request(this.constants.GRAPHQL_ENDPOINT[this.wallet.networkID], query).then(
+      (data: QueryResult) => this.handleData(data)
+    );
+
+    return true;
+  }
+
+  async handleData(data: QueryResult) {
+    for (let t in data) {
+      if (data[t] != null) {
+        this.data[parseInt(t.substring(1))] = parseFloat(
+          data[t].xMPHRewardDistributed
+        );
+      } else {
+        this.data[parseInt(t.substring(1))] = 0;
+      }
+    }
+  }
+}
+
+interface QueryResult {
+  globalStats: {
+    xMPHRewardDistributed: string;
+  };
+}
