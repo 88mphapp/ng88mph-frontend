392c36d2164d622b981fcf04857f9e55529a2688
0xSzeth <0xSzeth@gmail.com>
2021-10-08 11:09:49 -0600

    Fix block explorer link for multi-chain deployment

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -10,11 +10,13 @@ export class ConstantsService {
     MAINNET: 1,
     RINKEBY: 4,
     POLYGON: 137,
+    AVALANCHE: 43114,
   };
   CHAIN_ID_STRING = {
     [this.CHAIN_ID.MAINNET]: 'mainnet',
     [this.CHAIN_ID.RINKEBY]: 'rinkeby',
     [this.CHAIN_ID.POLYGON]: 'polygon',
+    [this.CHAIN_ID.AVALANCHE]: 'avalanche',
   };
   NETWORK_METADATA = {
     [this.CHAIN_ID.MAINNET]: {
@@ -26,7 +28,7 @@ export class ConstantsService {
         decimals: 18,
       },
       rpcUrls: ['https://mainnet.infura.io/v3'],
-      blockExplorerUrls: ['https://etherscan.com'],
+      blockExplorerUrls: ['https://etherscan.io'],
     },
     [this.CHAIN_ID.RINKEBY]: {
       chainId: '0x4',
@@ -37,7 +39,7 @@ export class ConstantsService {
         decimals: 18,
       },
       rpcUrls: ['https://rinkeby.infura.io/v3'],
-      blockExplorerUrls: ['https://rinkeby.etherscan.com'],
+      blockExplorerUrls: ['https://rinkeby.etherscan.io'],
     },
     [this.CHAIN_ID.POLYGON]: {
       chainId: '0x89',
@@ -50,6 +52,17 @@ export class ConstantsService {
       rpcUrls: ['https://polygon-rpc.com'],
       blockExplorerUrls: ['https://polygonscan.com'],
     },
+    [this.CHAIN_ID.AVALANCHE]: {
+      chainId: '0xa86a',
+      chainName: 'Avalanche Network',
+      nativeCurrency: {
+        name: 'Avalanche',
+        symbol: 'AVAX',
+        decimals: 9,
+      },
+      rpcUrls: ['https://api.avax.network/ext/bc/C/rpc'],
+      blockExplorerUrls: ['https://cchain.explorer.avax.network'],
+    },
   };
 
   // GRAPHQL ENDPOINTS //
@@ -60,12 +73,14 @@ export class ConstantsService {
       'https://api.thegraph.com/subgraphs/name/bacon-labs/eighty-eight-mph-v3-rinkeby',
     [this.CHAIN_ID.POLYGON]:
       'https://api.thegraph.com/subgraphs/name/bacon-labs/eighty-eight-mph-v3-polygon',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   GRAPHQL_ENDPOINT_V2 = {
     [this.CHAIN_ID.MAINNET]:
       'https://api.thegraph.com/subgraphs/name/bacon-labs/eighty-eight-mph',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   MPH_TOKEN_GRAPHQL_ENDPOINT = {
     [this.CHAIN_ID.MAINNET]:
@@ -73,6 +88,7 @@ export class ConstantsService {
     [this.CHAIN_ID.RINKEBY]:
       'https://api.thegraph.com/subgraphs/name/0xszeth/mph-token-rinkeby',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BACK_TO_THE_FUTURE_GRAPHQL_ENDPOINT = {
     [this.CHAIN_ID.MAINNET]:
@@ -81,6 +97,7 @@ export class ConstantsService {
       'https://api.thegraph.com/subgraphs/name/0xszeth/back-to-the-future-rinkeby',
     [this.CHAIN_ID.POLYGON]:
       'https://api.thegraph.com/subgraphs/name/0xszeth/back-to-the-future-polygon',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BLOCKS_GRAPHQL_ENDPOINT = {
     [this.CHAIN_ID.MAINNET]:
@@ -89,12 +106,14 @@ export class ConstantsService {
       'https://api.thegraph.com/subgraphs/name/blocklytics/rinkeby-blocks',
     [this.CHAIN_ID.POLYGON]:
       'https://api.thegraph.com/subgraphs/name/elkfinance/matic-blocks',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CHAINLINK_GRAPHQL_ENDPOINT = {
     [this.CHAIN_ID.MAINNET]:
       'https://api.thegraph.com/subgraphs/name/openpredict/chainlink-prices-subgraph',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   UNISWAP_V2_GRAPHQL_ENDPOINT =
     'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2';
@@ -111,6 +130,7 @@ export class ConstantsService {
       'https://eth-rinkeby.alchemyapi.io/v2/2LxgvUYd5FzgiXVoAWlq-KyM4v-E7KJ4',
     [this.CHAIN_ID.POLYGON]:
       'https://polygon-mainnet.g.alchemy.com/v2/q7fqBluE1Tn5t76RmCeylJnF7NPfvN7P',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   RPC_WS = {
     [this.CHAIN_ID.MAINNET]:
@@ -119,6 +139,7 @@ export class ConstantsService {
       'wss://eth-rinkeby.ws.alchemyapi.io/v2/2LxgvUYd5FzgiXVoAWlq-KyM4v-E7KJ4',
     [this.CHAIN_ID.POLYGON]:
       'wss://polygon-mainnet.g.alchemy.com/v2/q7fqBluE1Tn5t76RmCeylJnF7NPfvN7P',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
 
   // API KEYS //
@@ -148,31 +169,37 @@ export class ConstantsService {
     [this.CHAIN_ID.MAINNET]: '0x56f34826Cc63151f74FA8f701E4f73C5EAae52AD',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   DEV_WALLET = {
     [this.CHAIN_ID.MAINNET]: '0xfecBad5D60725EB6fd10f8936e02fa203fd27E4b',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   MERKLE_DISTRIBUTOR = {
     [this.CHAIN_ID.MAINNET]: '0x8c5ddBB0fd86B6480D81A1a5872a63812099C043',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   MPH_ADDRESS = {
     [this.CHAIN_ID.MAINNET]: '0x8888801af4d980682e47f1a9036e589479e835c5',
     [this.CHAIN_ID.RINKEBY]: '0xC79a56Af51Ec36738E965e88100e4570c5C77A93',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   XMPH_ADDRESS = {
     [this.CHAIN_ID.MAINNET]: '0x1702F18c1173b791900F81EbaE59B908Da8F689b',
     [this.CHAIN_ID.RINKEBY]: '0x59EE65726f0b886Ec924271B51A3c1e78F52d1FB',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   V2_REWARDS = {
     [this.CHAIN_ID.MAINNET]: '0x98df8D9E56b51e4Ea8AA9b57F8A5Df7A044234e1',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
 
   // EXTERNAL CONTRACTS //
@@ -187,16 +214,19 @@ export class ConstantsService {
     [this.CHAIN_ID.MAINNET]: '0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   SUSHI_MPH_REWARDER = {
     [this.CHAIN_ID.MAINNET]: '0x2889fE6B8cfAE15CC53157b8f0494FdB08721C39',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   SUSHI_MPH_REWARDER_ID = {
     [this.CHAIN_ID.MAINNET]: 4,
     [this.CHAIN_ID.RINKEBY]: null,
     [this.CHAIN_ID.POLYGON]: null,
+    [this.CHAIN_ID.AVALANCHE]: null,
   };
 
   // TOKENS //
@@ -204,151 +234,181 @@ export class ConstantsService {
     [this.CHAIN_ID.MAINNET]: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BAL = {
     [this.CHAIN_ID.MAINNET]: '0xba100000625a3754423978a60c9317c58a424e3d',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BAT = {
     [this.CHAIN_ID.MAINNET]: '0x0d8775f648430679a709e98d2b0cb6250d2887ef',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BNT = {
     [this.CHAIN_ID.MAINNET]: '0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BTCCRV = {
     [this.CHAIN_ID.MAINNET]: '',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '0xf8a57c1d3b9629b77b6726a042ca48990a84fb49',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   COMP = {
     [this.CHAIN_ID.MAINNET]: '0xc00e94cb662c3520282e6f5717214004a7f26888',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRV = {
     [this.CHAIN_ID.MAINNET]: '0xd533a949740bb3306d119cc777fa900ba034cd52',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRVRENWBTC = {
     [this.CHAIN_ID.MAINNET]: '0x49849c98ae39fff122806c06791fa73784fb3675',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRVUSDBTCETH = {
     [this.CHAIN_ID.MAINNET]: '',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '0xdad97f7713ae9437fa9249920ec8507e5fbb23d3',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   DAI = {
     [this.CHAIN_ID.MAINNET]: '0x6b175474e89094c44da98b954eedeac495271d0f',
     [this.CHAIN_ID.RINKEBY]: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea',
     [this.CHAIN_ID.POLYGON]: '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   FARM = {
     [this.CHAIN_ID.MAINNET]: '0xa0246c9032bC3A600820415aE600c6388619A14D',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   FTT = {
     [this.CHAIN_ID.MAINNET]: '0x50d1c9771902476076ecfc8b2a83ad6b9355a4c9',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   GUSD = {
     [this.CHAIN_ID.MAINNET]: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   LINK = {
     [this.CHAIN_ID.MAINNET]: '0x514910771af9ca656af840dff83e8264ecf986ca',
     [this.CHAIN_ID.RINKEBY]: '0x01be23585060835e02b77ef475b0cc51aa1e0709',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   RAI = {
     [this.CHAIN_ID.MAINNET]: '0x03ab458634910aad20ef5f1c8ee96f1d6ac54919',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   REN = {
     [this.CHAIN_ID.MAINNET]: '0x408e41876cccdc0f92210600ef50372656052a38',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   SNX = {
     [this.CHAIN_ID.MAINNET]: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   STKAAVE = {
     [this.CHAIN_ID.MAINNET]: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   SUSD = {
     [this.CHAIN_ID.MAINNET]: '0x57ab1ec28d129707052df4df418d58a2d46d5f51',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   SUSHI = {
     [this.CHAIN_ID.MAINNET]: '0x6b3595068778dd592e39a122f4f5a5cf09c90fe2',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   TUSD = {
     [this.CHAIN_ID.MAINNET]: '0x0000000000085d4780b73119b644ae5ecd22b376',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   UNI = {
     [this.CHAIN_ID.MAINNET]: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   USDC = {
     [this.CHAIN_ID.MAINNET]: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     [this.CHAIN_ID.RINKEBY]: '0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b',
     [this.CHAIN_ID.POLYGON]: '0x2791bca1f2de4661ed88a30c99a7a9449aa84174',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   USDP = {
     [this.CHAIN_ID.MAINNET]: '0x8e870d67f660d95d5be530380d0ec0bd388289e1',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   USDT = {
     [this.CHAIN_ID.MAINNET]: '0xdac17f958d2ee523a2206206994597c13d831ec7',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   WBTC = {
     [this.CHAIN_ID.MAINNET]: '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   WETH = {
     [this.CHAIN_ID.MAINNET]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   WMATIC = {
     [this.CHAIN_ID.MAINNET]: '',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   YFI = {
     [this.CHAIN_ID.MAINNET]: '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   ZRX = {
     [this.CHAIN_ID.MAINNET]: '0xe41d2489571d322189246dafa5ebde1f4699f498',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   ZERO_ADDR = '0x0000000000000000000000000000000000000000';
 
@@ -357,41 +417,49 @@ export class ConstantsService {
     [this.CHAIN_ID.MAINNET]: '0x06325440d014e39736583c165c2963ba99faf14e',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   ALINK = {
     [this.CHAIN_ID.MAINNET]: '0xa64bd6c70cb9051f6a9ba1f163fdc07e0dfb5f84',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRVOBTC = {
     [this.CHAIN_ID.MAINNET]: '0x2fe94ea3d5d4a175184081439753de15aef9d614',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRVHBTC = {
     [this.CHAIN_ID.MAINNET]: '0xb19059ebb43466c323583928285a49f558e572fd',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   THREECRV = {
     [this.CHAIN_ID.MAINNET]: '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRVHUSD = {
     [this.CHAIN_ID.MAINNET]: '0x5b5cfe992adac0c9d48e05854b2d91c73a003858',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   CRVRENWSBTC = {
     [this.CHAIN_ID.MAINNET]: '0x075b1bb99792c9e1041ba13afef80c91a1e70fb3',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   YCRV = {
     [this.CHAIN_ID.MAINNET]: '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
 
   // LP TOKENS //
@@ -399,21 +467,25 @@ export class ConstantsService {
     [this.CHAIN_ID.MAINNET]: '0x4d96369002fc5b9687ee924d458a7e5baa5df34e',
     [this.CHAIN_ID.RINKEBY]: '0x52768abae4e3f4a714aee3d0f85b4eba6360fda3',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   UNISWAP_V3_LP = {
     [this.CHAIN_ID.MAINNET]: '0xda7dd9ad1b2af1e50c73a0cc5d23ec5397478763',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   SUSHISWAP_LP = {
     [this.CHAIN_ID.MAINNET]: '0xb2c29e311916a346304f83aa44527092d5bd4f0f',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
   BANCOR_LP = {
     [this.CHAIN_ID.MAINNET]: '0xdf3fdfbce72da4fd42e7cfde7249e15357c7d808',
     [this.CHAIN_ID.RINKEBY]: '',
     [this.CHAIN_ID.POLYGON]: '',
+    [this.CHAIN_ID.AVALANCHE]: '',
   };
 
   SUSHI_LP = '0xb2c29e311916a346304f83aa44527092d5bd4f0f';
