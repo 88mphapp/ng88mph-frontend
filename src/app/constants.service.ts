00ff8a9c51db520d59c1ce05c4961bec393b5b84
0xSzeth <0xSzeth@gmail.com>
2021-12-19 12:36:32 -0700

    Use more reliable blocks subgraphs for Fantom and Avalanche

--- src/app/constants.service.ts
+++ src/app/constants.service.ts
@@ -131,9 +131,9 @@ export class ConstantsService {
     [this.CHAIN_ID.POLYGON]:
       'https://api.thegraph.com/subgraphs/name/elkfinance/matic-blocks',
     [this.CHAIN_ID.AVALANCHE]:
-      'https://api.thegraph.com/subgraphs/name/dasconnor/avalanche-blocks',
+      'https://api.thegraph.com/subgraphs/name/elkfinance/avax-blocks',
     [this.CHAIN_ID.FANTOM]:
-      'https://api.thegraph.com/subgraphs/name/0xfin/fantomblocks',
+      'https://api.thegraph.com/subgraphs/name/elkfinance/ftm-blocks',
     [this.CHAIN_ID.V2]:
       'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
   };
