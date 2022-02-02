ea6c646d0401be9ddaac369943222a04e54daa71
0xSzeth <0xSzeth@gmail.com>
2022-02-01 17:46:17 -0700

    Add veMPH stats and setup for create/add/extend veMPH lock

--- src/app/gauge/gauge.component.ts
+++ src/app/gauge/gauge.component.ts
@@ -1,3 +1,7 @@
+// TODO
+// 1- Change veMPH address to correct address after deployment (contracts.json and constants.service.ts)
+// 2- Change MPHGaugeController address to correct address after deployment (contracts.json)
+
 import { Component, OnInit, NgZone } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalUnstakeComponent } from './modal-unstake/modal-unstake.component';
@@ -6,6 +10,8 @@ import { ConstantsService } from '../constants.service';
 import { ContractService } from '../contract.service';
 import { DataService } from '../data.service';
 import { WalletService } from '../wallet.service';
+import { HelpersService } from '../helpers.service';
+import { Timer } from '../timer';
 
 @Component({
   selector: 'app-gauge',
@@ -14,18 +20,31 @@ import { WalletService } from '../wallet.service';
 })
 export class GaugeComponent implements OnInit {
   // user variables
-  mphLocked: BigNumber;
-  mphBalance: BigNumber;
-  mphUnlocked: BigNumber;
-  veBalance: BigNumber;
-  lockEnd: number;
+  mphLocked: BigNumber; // amount of MPH user has locked
+  mphBalance: BigNumber; // user's MPH balance in wallet
+  mphAllowance: BigNumber; // user's MPH allowance for the veMPH contract
+  mphUnlocked: BigNumber; // amount of MPH user can unlock
+  veBalance: BigNumber; // user's veMPH balance
+  lockEnd: number; // timestamp when user's lock ends
+  lockAmount: BigNumber; // amount of MPH to lock
+  lockDuration: number; // days to lock
 
   // global variables
+  veTotal: BigNumber;
+  totalMPHSupply: BigNumber;
+  circulatingMPHSupply: BigNumber; // circulating = total - gov treasury - dev wallet - merkle distributor
+  totalMPHLocked: BigNumber;
+  averageLock: BigNumber;
+
+  // gauge
+  timeLeft: number;
+  timeLeftCountdown: any;
 
   constructor(
     public constants: ConstantsService,
     public contract: ContractService,
     public datas: DataService,
+    public helpers: HelpersService,
     public wallet: WalletService,
     private modalService: NgbModal,
     private zone: NgZone
@@ -72,19 +91,34 @@ export class GaugeComponent implements OnInit {
     if (resetUser) {
       this.mphLocked = new BigNumber(0);
       this.mphBalance = new BigNumber(0);
+      this.mphAllowance = new BigNumber(0);
       this.mphUnlocked = new BigNumber(0);
       this.veBalance = new BigNumber(0);
       this.lockEnd = 0;
+      this.lockAmount = new BigNumber(0);
+      this.lockDuration = 7;
     }
 
     if (resetGlobal) {
+      this.veTotal = new BigNumber(0);
+      this.totalMPHSupply = new BigNumber(0);
+      this.circulatingMPHSupply = new BigNumber(0);
+      this.totalMPHLocked = new BigNumber(0);
+      this.averageLock = new BigNumber(0);
+
+      // gauge
+      this.timeLeft = 0;
     }
   }
 
-  loadData(loadUser: boolean, loadGlobal: boolean): void {
+  async loadData(loadUser: boolean, loadGlobal: boolean) {
     const web3 = this.wallet.httpsWeb3(this.wallet.networkID);
     const mph = this.contract.getNamedContract('MPHToken', web3);
-    const vemph = this.contract.getNamedContract('veMPH', web3); // @dev currently using address for veCRV during development
+    const vemph = this.contract.getNamedContract('veMPH', web3);
+    const gaugeController = this.contract.getNamedContract(
+      'MPHGaugeController',
+      web3
+    ); // @dev currently using address for FXSGaugeController during development
     const address = this.wallet.actualAddress.toLowerCase();
 
     if (loadUser && address) {
@@ -95,6 +129,16 @@ export class GaugeComponent implements OnInit {
           .call()
           .then((balance) => {
             this.mphBalance = new BigNumber(balance).div(
+              this.constants.PRECISION
+            );
+            this.setLockAmount(this.mphBalance);
+          });
+
+        mph.methods
+          .allowance(address, vemph.options.address)
+          .call()
+          .then((result) => {
+            this.mphAllowance = new BigNumber(result).div(
               this.constants.PRECISION
             );
           });
@@ -130,8 +174,134 @@ export class GaugeComponent implements OnInit {
     }
 
     if (loadGlobal) {
-      console.log('load global');
+      // fetch total and circulating MPH supply
+      mph.methods
+        .totalSupply()
+        .call()
+        .then((result) => {
+          const supply = new BigNumber(result).div(this.constants.PRECISION);
+          this.totalMPHSupply = supply;
+          this.circulatingMPHSupply = this.circulatingMPHSupply.plus(supply);
+        });
+      mph.methods
+        .balanceOf(this.constants.GOV_TREASURY[this.wallet.networkID])
+        .call()
+        .then((result) => {
+          const balance = new BigNumber(result).div(this.constants.PRECISION);
+          this.circulatingMPHSupply = this.circulatingMPHSupply.minus(balance);
+        });
+      mph.methods
+        .balanceOf(this.constants.DEV_WALLET[this.wallet.networkID])
+        .call()
+        .then((result) => {
+          const balance = new BigNumber(result).div(this.constants.PRECISION);
+          this.circulatingMPHSupply = this.circulatingMPHSupply.minus(balance);
+        });
+      mph.methods
+        .balanceOf(this.constants.MERKLE_DISTRIBUTOR[this.wallet.networkID])
+        .call()
+        .then((result) => {
+          const balance = new BigNumber(result).div(this.constants.PRECISION);
+          this.circulatingMPHSupply = this.circulatingMPHSupply.minus(balance);
+        });
+
+      // load gauge time left
+      gaugeController.methods
+        .time_total()
+        .call()
+        .then((result) => {
+          this.timeLeft = parseInt(result);
+          this.timeLeftCountdown = new Timer(this.timeLeft, 'down');
+          this.timeLeftCountdown.start();
+        });
+
+      await vemph.methods
+        .totalSupply()
+        .call()
+        .then((result) => {
+          this.veTotal = new BigNumber(result).div(this.constants.PRECISION);
+        });
+
+      // @dev totalFXSSupply() returns a slightly different value than supply()
+      await vemph.methods
+        .supply()
+        .call()
+        .then((result) => {
+          this.totalMPHLocked = new BigNumber(result).div(
+            this.constants.PRECISION
+          );
+        });
+
+      // @dev MPH * (3/4t + 1) = veMPH
+      this.averageLock = this.veTotal
+        .div(this.totalMPHLocked)
+        .minus(1)
+        .times(4)
+        .div(3);
     }
+  }
+
+  setLockAmount(amount: string | number | BigNumber): void {
+    this.lockAmount = new BigNumber(amount);
+    if (this.lockAmount.isNaN()) {
+      this.lockAmount = new BigNumber(0);
+    }
+  }
+
+  presetLockAmount(percent: string | number): void {
+    const ratio = new BigNumber(percent).div(100);
+    this.lockAmount = this.mphBalance.times(ratio);
+  }
+
+  setLockDuration(duration: string | number): void {
+    this.lockDuration = +duration;
+    if (isNaN(this.lockDuration)) {
+      this.lockDuration = 0;
+    }
+  }
+
+  approve(): void {
+    const user = this.wallet.actualAddress;
+    const mph = this.contract.getNamedContract('MPHToken');
+    const lockAmount = this.helpers.processWeb3Number(
+      this.lockAmount.times(this.constants.PRECISION)
+    );
+
+    this.wallet.approveToken(
+      mph,
+      this.constants.VEMPH_ADDRESS[this.wallet.networkID],
+      lockAmount,
+      () => {},
+      () => {},
+      async () => {
+        const web3 = this.wallet.httpsWeb3();
+        const mph = this.contract.getNamedContract('MPHToken', web3);
+        await mph.methods
+          .allowance(user, this.constants.VEMPH_ADDRESS[this.wallet.networkID])
+          .call()
+          .then((result) => {
+            this.mphAllowance = new BigNumber(result).div(
+              this.constants.PRECISION
+            );
+          });
+      },
+      (error) => {
+        this.wallet.displayGenericError(error);
+      },
+      true
+    );
+  }
+
+  createLock(): void {
+    console.log('creating lock...');
+  }
+
+  increaseLockAmount(): void {
+    console.log('increasing lock amount...');
+  }
+
+  increaseLockDuration(): void {
+    console.log('increasing lock duration...');
   }
 
   timestampToDateString(timestampSec: number): string {
